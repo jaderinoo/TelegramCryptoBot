427682ad379ec12470487f3df6d57d074f395385
Jad El-Khatib <jaderinoo@gmail.com>
2019-04-21 20:20:01 -0400

    Testing environment with example program

--- TelegramCryptoBot/main.py
+++ TelegramCryptoBot/main.py
@@ -1,1 +1,19 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+def price(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+def main():
+    updater = Updater('886915510:AAHEIaXLNAFdmLDv6qHwotAAMv6ty_BQx7I')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('price',price))
+    updater.start_polling()
+    updater.idle()
+if __name__ == '__main__':
+    main()
