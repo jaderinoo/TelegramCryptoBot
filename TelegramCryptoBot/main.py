80adcb1a9769660ce65cc8ec733b0b58cc4335cf
Jad El-Khatib <jaderinoo@gmail.com>
2019-06-23 19:49:14 -0400

    Changed token from BTC to SNTR, Format string next

--- TelegramCryptoBot/main.py
+++ TelegramCryptoBot/main.py
@@ -2,14 +2,23 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
-import json
+import json 
 
+#Format the String so that it looks 
+def msg(data):
+    
+    #Write code that will format the string
+    msg = data
+
+    return msg
+
+#Call api and grab coin info
+#Send coin info to msg to be formatted 
 def price(bot,update):
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
     parameters = {
-        'start':'1',
-        'limit':'1',
-        'convert':'USD'
+        'id':'2764'
     }
     headers = {
         'Accepts': 'application/json',
@@ -18,16 +27,14 @@ def price(bot,update):
 
     session = Session()
     session.headers.update(headers)
-
     response = session.get(url, params=parameters)
     data = json.loads(response.text)
-    
     chat_id = update.message.chat_id
     
     print(data)
-    bot.sendMessage(chat_id, data)
+    bot.sendMessage(chat_id,msg(data))
     
-
+#Initializes the telegram bot and listens for the /price command
 def main():
     updater = Updater('886915510:AAHEIaXLNAFdmLDv6qHwotAAMv6ty_BQx7I')
     dp = updater.dispatcher
@@ -35,7 +42,8 @@ def main():
     updater.start_polling()
     updater.idle()
     
+    
 if __name__ == '__main__':
-     main()
+    main()
         
         
