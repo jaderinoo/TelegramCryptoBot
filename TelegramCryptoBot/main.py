eb2e1442b47174e7471682733f255660bd624788
Jad El-Khatib <jaderinoo@gmail.com>
2019-06-23 18:58:26 -0400

    Shows the current price of BTC when /Price is typed to the bot

--- TelegramCryptoBot/main.py
+++ TelegramCryptoBot/main.py
@@ -1,39 +1,41 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import requests
-import re
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
 import json
 
-def get_url():
+def price(bot,update):
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
     parameters = {
         'start':'1',
-        'limit':'5',
+        'limit':'1',
         'convert':'USD'
     }
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': '---',
         }
-    
-
-    return url
-def price(url, headers, parameters):
 
     session = Session()
     session.headers.update(headers)
+
     response = session.get(url, params=parameters)
     data = json.loads(response.text)
-        
-    data = json.loads(response.text)
-    print(data)
     
+    chat_id = update.message.chat_id
+    
+    print(data)
+    bot.sendMessage(chat_id, data)
+    
+
 def main():
     updater = Updater('886915510:AAHEIaXLNAFdmLDv6qHwotAAMv6ty_BQx7I')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('price',price))
     updater.start_polling()
     updater.idle()
+    
 if __name__ == '__main__':
-    main()
+     main()
+        
+        
