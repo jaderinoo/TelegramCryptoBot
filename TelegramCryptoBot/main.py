b62b4531e884359f9a08827ea613abbbb976da99
Jad El-Khatib <jaderinoo@gmail.com>
2019-07-03 23:07:00 -0400

    Posts the name, symbol, circulating supply, total, and price

--- TelegramCryptoBot/main.py
+++ TelegramCryptoBot/main.py
@@ -17,25 +17,34 @@ def price(bot,update):
         'X-CMC_PRO_API_KEY': 'db5a55e7-7cd4-4bf9-8ee1-f2e71239a70c',
         }
 
+    #Start session
     session = Session()
     
+    #Set headers and pull response while pulling data
     session.headers.update(headers)
-    
     response = session.get(url, params=parameters)
-
     data = response.json()
     
-    print(data['data']['2764']['name'])
+    #Format the response and sets it to an array
+    name = data['data']['2764']['name']
+    symbol = data['data']['2764']['symbol']
+    circ = data['data']['2764']['circulating_supply']
+    total = data['data']['2764']['total_supply']
+    price = data['data']['2764']['quote']['USD']['price']
+    price = ('%.08f' % price)
     
+    currencydata = [name,symbol,circ,total,price]
+    
+    #Pull chat ID
     chat_id = update.message.chat_id
  
-    #for element in data['data']:
-    #    print(element)
-
-    #print data["data"]["name"]
-    #print(data)
-    bot.sendMessage(chat_id,data)
+    #Post message locally
+    print(currencydata[0])
+    print(currencydata[1])
     
+    #Send message to bot
+    bot.sendMessage(chat_id, currencydata, sep = "\n")
+
 #Initializes the telegram bot and listens for the /price command
 def main():
     updater = Updater('886915510:AAHEIaXLNAFdmLDv6qHwotAAMv6ty_BQx7I')
